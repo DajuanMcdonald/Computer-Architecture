21e028f8544dab46cd9422da52c72d740e3a0db7
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-10-29 18:33:18 -0400

    working on run method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,6 +9,7 @@ class CPU:
         """Construct a new CPU."""
         self.memory = bytearray(256)
         self.registers = bytearray(8)
+        self.pc = 0
         
 
     def load(self):
@@ -21,10 +22,10 @@ class CPU:
         program = [
             # From print8.ls8
             0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
+            0b00000000, # NOP
+            0b00001000, # ?
             0b01000111, # PRN R0
-            0b00000000,
+            0b00000000, # NOP
             0b00000001, # HLT
         ]
 
@@ -39,6 +40,8 @@ class CPU:
         if op == "ADD":
             self.registers[reg_a] += self.registers[reg_b]
         #elif op == "SUB": etc
+        elif op == "SUB":
+            self.registers[reg_a] -= self.registers[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,15 +65,41 @@ class CPU:
 
         print()
 
-    def ram_read(self, address, value):
+    def ram_read(self, address):
         """ should accept the address to read and return the value stored """
-        return self.memory[address].pop(value)
+        return self.memory[address]
 
-    def ram_write(self, address, value):
+    def ram_write(self, address, data):
         """ should accept the value to write and the address to write to """
-        self.memory[address].append(value)
+        self.memory[address] = data
 
 
     def run(self):
         """Run the CPU."""
-        pass
+        IR = self.pc
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        running = True
+
+        while running:
+            instruction = self.ram_read(IR)
+            op_a = self.ram_read(IR + 1)
+            op_b = self.ram_read(IR + 2)
+
+            if instruction == LDI:
+                self.registers[op_a] == op_b
+
+                IR += 3
+
+            elif instruction == PRN:
+                print(self.registers[op_a])
+                IR += 2
+
+            elif instruction == HLT:
+                sys.exit(0)
+
+            else:
+                print(f'Unknown instruction at: {IR}')
+                sys.exit(1)
