c74f5794e2b94cab6c6c326a49fa228e8102bb13
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-10-30 04:03:27 -0400

    modified load() to accept read in file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,25 +6,34 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
+        """ When the LS-8 CPU is booted, the following steps occur: """
         """
         ## Registers
-
         # 8 general-purpose 8-bit numeric registers R0-R7.
+
+        * `R0`-`R6` are cleared to `0`.
+        * `R7` is set to `0xF4`.
+        * `PC` and `FL` registers are cleared to `0`.
+        * RAM is cleared to `0`.
+
         # * R5 is reserved as the interrupt mask (IM)
         # * R6 is reserved as the interrupt status (IS)
         # * R7 is reserved as the stack pointer (SP)
         """
         self.registers = bytearray(8)
+
+
         """
-        ## Stack
         # total of 256 bytes of memory
         """
         self.memory = bytearray(256)
 
+
         """
         The SP points at the value at the top of the stack (most recently pushed), or at
         address `F4` if the stack is empty.
+        
+        The value of the key pressed is stored in address `0xF4`
         
         """
         self.registers[SP] = 0xF4
@@ -37,43 +46,69 @@ class CPU:
         self.IR = 0
 
         """
-        #  `FL`: Flags, holds the current flags status
+        # `FL`: Flags, holds the current flags status
         # These flags can change based on the operands given to the `CMP` opcode.
         # If a particular bit is set, that flag is "true"
         """
         self.FL = 0
 
 
+        """
+        Some instructions set the PC directly. These are:
+        * CALL : Calls a subroutine (function) at the address stored in the register.
+        * INT : 
+        * IRET
+        * LDI : Set the value of a register to an integer.
+        * JMP
+        * JNE
+        * JEQ
+        * JGT
+        * JGE
+        * JLT
+        * JLE
+        * RET
+        * HLT : Halt the CPU (and exit the emulator).
+        * PRN : Print numeric value stored in the given register.
+        * MUL : Multiply the values in two registers together and store the result in registerA.
+
+        * CMP : Compare the values in two registers.
+        """
+        self.INSTRUCTION_SET = {
+            'CALL' : 0b01010000,
+            'INT' : 0b01010010,
+            'MUL' : 0b10100010,
+            'PRN' : 0b01000111,
+            'LDI' : 0b10000010,
+            'HLT' : 0b00000001,
+
+        }
+
+
+
         
+
+    def load(self, program):
+
+        """Load a program into memory."""
+
+        address = 0
+        
+        for instruction in program:
+            if instruction:
+                instruction = instruction.split()[0]
+                if instruction[0] != '#':
+                    self.memory[address] = int(instruction, 2)
+                    address += 1
+
     def ram_read(self, data):
         """ should accept the address to read and return the value stored """
         return self.memory[data]
 
+
     def ram_write(self, address, data):
         """ should accept the value to write and the address to write to """
         self.memory[address] = data
-
-    def load(self):
-        """Load a program into memory."""
-
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000, # NOP R0
-            0b00001000, # LDI R0, 8
-            0b01000111, # PRN R0
-            0b00000000, # NOP R0
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.memory[address] = instruction
-            address += 1
-
+        
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -83,6 +118,8 @@ class CPU:
         #elif op == "SUB": etc
         elif op == "SUB":
             self.registers[reg_a] -= self.registers[reg_b]
+        elif op == "MUL":
+            self.registers[reg_a] *= self.registers[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -104,7 +141,7 @@ class CPU:
         for i in range(8):
             print(" %02X" % self.registers[i], end='')
 
-        print()
+        print(sys.argv)
 
 
 
